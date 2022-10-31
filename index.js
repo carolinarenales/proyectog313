878974ee8adc52dd567c3f72b4a8944285bc16dc
carolinarenales <116692871+carolinarenales@users.noreply.github.com>
2022-10-31 15:42:43 -0500

    Commit N 1

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from 'cors'
+//import blogRoutes from './routes/routes.js'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+//app.use('/usuarios',blogRoutes)
+
+
+app.get('/',(req,res)=>{
+    res.send('😎HOLA MUNDO G31 😍')
+})
+
+app.listen(8000, ()=> {
+    console.log('Servidor Corriendo en  http://localhost:8000/')
+})
